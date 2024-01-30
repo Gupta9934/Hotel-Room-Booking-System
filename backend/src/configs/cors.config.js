fae424db496fc94988f3da3eaa15862d94a00f75
Sanjeeb Tiwary <saiyam9934gupta@gmail.com>
2024-01-30 15:41:21 +0530

    Update

---
+++ backend/src/configs/cors.config.js
@@ -0,0 +1,27 @@
+/**
+ * @name Hotel Room Booking System
+ * @author Md. Samiur Rahman (Mukul)
+ * @description Hotel Room Booking and Management System Software ~ Developed By Md. Samiur Rahman (Mukul)
+ * @copyright ©2023 ― Md. Samiur Rahman (Mukul). All rights reserved.
+ * @version v0.0.1
+ *
+ */
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS origin'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+module.exports = corsOptions;
